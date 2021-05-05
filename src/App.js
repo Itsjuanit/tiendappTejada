b11e8d2575a2cd840cc89d56741321a7946fe9d0
Juan Ignacio Tejada <tejadajuanignacio12mail.com>
2021-05-04 21:53:05 -0300

    New app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo" width="300"/>
+        <p>Desafio de CODER HOUSE</p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.coderhouse.com/online/reactjs"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          APRENDE REACT
         </a>
+
+      <a
+        className="juani"
+        href="https://portfolio-itsjuanit.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Itsjuanit
+      </a>
       </header>
     </div>
   );
